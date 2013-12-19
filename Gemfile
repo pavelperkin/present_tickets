96a564de402db4a29896a96239525c4e060e1ca9
pavelperkin <perkinpv@mail.ru>
2013-12-19 23:04:46 +0200

    deploy

--- Gemfile
+++ Gemfile
@@ -4,11 +4,14 @@ source 'https://rubygems.org'
 gem 'rails', '4.0.2'
 
 # Use sqlite3 as the database for Active Record
-gem 'sqlite3'
+# gem 'sqlite3'
+gem 'pg'
 
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 4.0.0'
 gem 'devise'
+gem "therubyracer"
+gem "twitter-bootstrap-rails"
 
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -2,58 +2,31 @@ class TicketsController < ApplicationController
   before_filter :authenticate_user!
   before_action :set_ticket, only: [:show, :edit, :update, :destroy]
 
-  # GET /tickets
-  # GET /tickets.json
   def index
     @tickets = Ticket.all
+    @not_me = User.all.select {|u| u.id != current_user.id}.first
+    @tickets_my = current_user.tickets
+    @tickets_not_my = @not_me.tickets
   end
 
-  # GET /tickets/1
-  # GET /tickets/1.json
-  def show
-  end
-
-  # GET /tickets/new
   def new
     @ticket = Ticket.new
   end
 
-  # GET /tickets/1/edit
-  def edit
-  end
-
-  # POST /tickets
-  # POST /tickets.json
   def create
     @ticket = Ticket.new(ticket_params)
-
-    respond_to do |format|
-      if @ticket.save
-        format.html { redirect_to @ticket, notice: 'Ticket was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @ticket }
-      else
+    if @ticket.save
+      redirect_to root_path
+    else
         format.html { render action: 'new' }
-        format.json { render json: @ticket.errors, status: :unprocessable_entity }
-      end
     end
   end
 
-  # PATCH/PUT /tickets/1
-  # PATCH/PUT /tickets/1.json
   def update
-    respond_to do |format|
-      if @ticket.update(ticket_params)
-        format.html { redirect_to @ticket, notice: 'Ticket was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @ticket.errors, status: :unprocessable_entity }
-      end
-    end
+    @ticket.update(ticket_params)
+    redirect_to root_path
   end
 
-  # DELETE /tickets/1
-  # DELETE /tickets/1.json
   def destroy
     @ticket.destroy
     respond_to do |format|
